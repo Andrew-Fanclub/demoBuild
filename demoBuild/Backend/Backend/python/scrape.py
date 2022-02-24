865602db7803963390fd097d7f35f1d419bd96d5
4-gent <40777454+4-gent@users.noreply.github.com>
2022-02-24 00:31:01 -0800

    Delete demoBuild directory

--- demoBuild/Backend/Backend/python/scrape.py
+++
@@ -1,8 +0,0 @@
-# This file is the python web scraper
-# Needs to communicate to sql first then it will send data to frontend
-# From the database
-
-from urllib.request import urlopen as ureq
-from bs4 import BeautifulSoup as soup
-
-
