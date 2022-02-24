aa33368b6c626260d01d3e38c4ca9be0a3ddf15d
4-gent <40777454+4-gent@users.noreply.github.com>
2022-02-24 00:19:12 -0800

    Add files via upload

---
+++ demoBuild/Backend/Backend/node/mailer.js
@@ -0,0 +1,33 @@
+// mailer file for contact page
+// will use nodemailer and mailjet as api
+
+// side note need to wait for catherine to finish up frontend so I can connect it
+// also need to see if heroku will allow port-forwarding for mailjet api
+
+import { Mailer } from 'nodemailer-react';
+
+const transport = {
+    host: 'afcplushies@gmail.com',
+    secure: true,
+    auth: {user: 'afcplushies@gmail.com', pass: 'af2plush!3$'},
+}
+
+const defaults = {
+    from: 'body.req.email',
+}
+
+export const email = () => ({
+    subject: body.req.email,
+    body: (
+        'body.req.message'
+    )
+})
+
+export const mailer = Mailer(
+    { transport, defaults },
+    { email }
+)
+
+mailer.send('email', {
+    to: 'afcplushies@gmail.com'
+})
---
+++ demoBuild/Website/Website/Website/README.md
@@ -0,0 +1,4 @@
+# Website
+Main Repo for the stuffs
+
+Keep in mind look at the different Readme I made called AFCReadme.md
